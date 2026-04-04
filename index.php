71fdec18db100305b06c700a0e1e265e576468aa
tloftus82 <tloftus@uru.soccer>
2026-04-04 14:40:52 -0500

    Fix iPhone landscape white bars using viewport-fit=cover and safe area insets

--- index.php
+++ index.php
@@ -37,7 +37,7 @@
 <html lang="en">
   <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, viewport-fit=cover">
     <title>Soccer Stats Tracker</title>
     <link rel="stylesheet" href="css.css?nocache=<?php echo time(); ?>">
   </head>
